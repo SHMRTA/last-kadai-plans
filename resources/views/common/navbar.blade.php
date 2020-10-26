bb1e6440fad92f19fcaf7226ddc762e3bfa9c254
SHR <shimryta@gmail.com>
2020-10-26 09:46:06 +0000

    user registration

---
+++ resources/views/common/navbar.blade.php
@@ -0,0 +1,6 @@
+<ul class="nav navbar-nav navbar-right">
+    {{-- ユーザ登録ページへのリンク --}}
+    <li>{!! link_to_route('signup.get','登録',[],['class' => 'nav-link']) !!}</li>
+    {{-- ログインページへのリンク --}}
+    <li><a href="#">ログイン</a></li>
+</ul>
---
+++ resources/views/registration.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+
+@section('content')
+    <div class="center jumborton">
+        <div class="text-center">
+            <h1>トップページ</h1>
+            {{-- ユーザ登録ページへのリンク --}}
+            {!! link_to_route('signup.get','ユーザ登録',[],['class => btn btn-lg btn-primary']) !!}
+        </div>
+    </div>
+@endsection
